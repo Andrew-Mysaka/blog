f910b1b4ecb2afd1742c72679549364dd9f89098
Andrew-Mysaka <andrewmysaka@gmail.com>
2024-01-29 20:25:14 +0200

    Adding The "Hero" Component

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,10 +1,19 @@
+import Image from "next/image";
 
+import classes from "./hero.module.css";
 
 function Hero() {
     return (
-        <>
-
-        </>
+        <section className={classes.hero}>
+            <Image className={classes.image}
+                   src={"/images/site/andrii.png"}
+                   alt={"An image showing Andrii"}
+                   width={300}
+                   height={300}
+            />
+            <h1>Hi, I'm Andrii</h1>
+            <p>I blog about web development - especially frontend frameworks like React.</p>
+        </section>
     );
 }
 
