91613e8d68b3094f4db1487f199a8619c35c2272
Andrew-Mysaka <andrewmysaka@gmail.com>
2024-02-03 19:09:37 +0200

    Sending Data From The Client To The API Route

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,23 +1,72 @@
 import classes from "./contact-form.module.css";
+import {useState} from "react";
+
+async function sendContactData(contactDetails) {
+    const response = await fetch('/api/contact', {
+        method: 'POST',
+        body: JSON.stringify(contactDetails),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || 'Something went wrong!');
+    }
+}
 
 function ContactForm(props) {
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredMessage, setEnteredMessage] = useState('');
+
+    async function sendMessageHandler(event) {
+        event.preventDefault();
+
+        await sendContactData({
+            email: enteredEmail,
+            name: enteredName,
+            message: enteredMessage
+        })
+    }
+
     return (
         <section className={classes.contact}>
             <h1>How can I help you?</h1>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={sendMessageHandler}>
                 <div className={classes.controls}>
                     <div className={classes.control}>
                         <label htmlFor={"email"}>Your Email</label>
-                        <input type={"email"} id={"email"} required/>
+                        <input
+                            type={"email"}
+                            id={"email"}
+                            required
+                            value={enteredEmail}
+                            onChange={event => setEnteredEmail(event.target.value)}
+                        />
                     </div>
                     <div className={classes.control}>
                         <label htmlFor={"name"}>Your Name</label>
-                        <input type={"text"} id={"name"} required/>
+                        <input
+                            type={"text"}
+                            id={"name"}
+                            required
+                            value={enteredName}
+                            onChange={event => setEnteredName(event.target.value)}
+                        />
                     </div>
                 </div>
                 <div className={classes.control}>
                     <label htmlFor={"message"}>Your Message</label>
-                    <textarea rows={5} id={"message"} required/>
+                    <textarea
+                        rows={5}
+                        id={"message"}
+                        required
+                        value={enteredMessage}
+                        onChange={event => setEnteredMessage(event.target.value)}
+                    />
                 </div>
 
                 <div className={classes.actions}>
